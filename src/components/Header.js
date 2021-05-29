9393c54b45fd8a8a5f3086f393c55af6f64c5245
Cory Bass <cory.bass.07@gmail.com>
2021-05-29 16:21:56 -0500

    gave user ability to logout and redirect to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,39 @@
-const Header = () => {
+import { connect } from "react-redux";
+
+const Header = ({ auth }) => {
+    const renderContent = () => {
+        switch (auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    };
+
     return (
         <nav>
             <div className="nav-wrapper">
                 <a className="left brand-logo" href="/">
                     Emaily
                 </a>
-                <ul className="right">
-                    <li>
-                        <a href="/">Login With Google</a>
-                    </li>
-                </ul>
+                <ul className="right">{renderContent()}</ul>
             </div>
         </nav>
     );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+    return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(Header);
