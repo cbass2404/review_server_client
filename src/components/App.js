5b15fecc1fec18cf3c379be1fa5fbd783a042b27
Cory Bass <cory.bass.07@gmail.com>
2021-05-29 16:25:09 -0500

    setup basic landing page

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,10 @@ import * as actions from "../actions";
 
 // local files
 import Header from "./Header";
-
+import Landing from "./Landing";
 // temp components
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 const NoMatch = () => <h2>Page not Found</h2>;
 
 const App = ({ fetchUser }) => {
